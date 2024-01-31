278180be20a60bc541f8c9be2560f28381f93554
flysha1 <2903229312@qq.com>
2024-01-31 14:11:18 +0800

    init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,14 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "FLysha's blog",
+  description: "FLysha的博客",
+  theme,
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
