69f384f7535ef5943e43492c48f8d9edef814122
flysha1 <2903229312@qq.com>
2024-02-01 14:50:09 +0800

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,8 +3,13 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/blog/",
-
   lang: "zh-CN",
+  head: [
+    [
+      "link", 
+      { rel: "icon", href: "favicon.ico" },
+    ],
+  ],
   title: "Flysha",
   description: "Flysha's blog",
   theme,
