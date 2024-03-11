2fd281271aa8bd2e09a45bb6e1cc024c998b07f5
flysha1 <2903229312@qq.com>
2024-03-11 16:07:48 +0800

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,19 +1,28 @@
+import { viteBundler } from "@vuepress/bundler-vite";
+import path from "path";
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir);
+}
 export default defineUserConfig({
   base: "/blog/",
   lang: "zh-CN",
-  head: [
-    [
-      "link", 
-      { rel: "icon", href: "favicon.ico" },
-    ],
-  ],
+  head: [["link", { rel: "icon", href: "favicon.ico" }]],
   title: "Flysha",
   description: "Flysha's blog",
   theme,
-
+  bundler: viteBundler({
+    viteOptions: {
+      resolve: {
+        alias: {
+          "/blog": _resolve("./public"),
+        },
+      },
+    },
+    vuePluginOptions: {},
+  }),
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
